b17720a10595b6272400d99fb5e60d58b3e8b2c9
nhatan20032 <119768785+nhatan20032@users.noreply.github.com>
2024-03-22 09:50:32 +0700

    update databse

--- Data/Configuration/AcademicPerformConfiguration.cs
+++ Data/Configuration/AcademicPerformConfiguration.cs
@@ -12,9 +12,6 @@ namespace Data.Configuration
             builder.HasKey(t => t.Id);
             builder.HasIndex(t => t.Id).IsUnique();
 
-            builder.Property(t => t.UserId);
-            builder.HasIndex(t => t.UserId);
-
             builder.Property(t => t.GradePointId);
             builder.HasIndex(t => t.GradePointId);
 
@@ -23,7 +20,6 @@ namespace Data.Configuration
             builder.Property(t => t.Status).HasMaxLength(5);
 
             //Set Relationship
-            builder.HasOne<User>(t => t.User).WithMany(t => t.AcademicPerformances).HasForeignKey(t => t.UserId);
             builder.HasOne<GradePoint>(t => t.GradePoint).WithMany(t => t.AcademicPerformances).HasForeignKey(t => t.GradePointId);
         }
     }
