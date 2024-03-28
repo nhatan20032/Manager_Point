875e0349e305e6d9ed729c224a29e69dd4a40edc
nhatan20032 <119768785+nhatan20032@users.noreply.github.com>
2024-03-28 22:31:47 +0700

    update code

--- Manager_Point/Models/Enum/Gender.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Manager_Point.Models.Enum
-{
-    public enum Gender
-    {
-        Male = 1,
-        Female = 2,
-        Other = 3
-    }
-}
--- Manager_Point/Models/Enum/TypeTeacher.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Manager_Point.Models.Enum
-{
-    public enum TypeTeacher
-    {
-        Homeroom_Teacher = 0,
-        Subject_Teacher = 1
-    }
-}
