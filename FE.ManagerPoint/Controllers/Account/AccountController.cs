4a0a2962ccafb3b7118ce1262e521a982ee1927a
nhatan20032 <119768785+nhatan20032@users.noreply.github.com>
2024-03-29 14:44:48 +0700

    update login page

---
+++ FE.ManagerPoint/Controllers/Account/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FE.ManagerPoint.Controllers.Account
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+    } 
+}
--- FE.ManagerPoint/Program.cs
+++ FE.ManagerPoint/Program.cs
@@ -4,6 +4,9 @@
 builder.Services.AddControllersWithViews();
 // F5 Reload lại giao diện khi Dev
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddSession();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -13,10 +16,10 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
+app.UseSession();
 app.UseRouting();
 
 app.UseAuthorization();
