a3c8927a7aa35f0013bd3fe56aa2f692224ba876
nhatan20032 <119768785+nhatan20032@users.noreply.github.com>
2024-04-14 18:28:26 +0700

    update code

--- FE.ManagerPoint/wwwroot/js/Teacher_Class/Teacher_Class.js
+++ FE.ManagerPoint/wwwroot/js/Teacher_Class/Teacher_Class.js
@@ -1,4 +1,93 @@
-﻿function createClass() {
+﻿function Get_Teacher_No_HomeRoom() {
+    this.$table = $('#teacher_table').DataTable({
+        "language": {
+            "sProcessing": "Đang xử lý...",
+            "sLengthMenu": "Xem _MENU_ mục",
+            "sZeroRecords": "Không tìm thấy dòng nào phù hợp",
+            "sInfo": "Đang xem _START_ đến _END_ trong tổng số _TOTAL_ mục",
+            "sInfoEmpty": "Đang xem 0 đến 0 trong tổng số 0 mục",
+            "sInfoFiltered": "(được lọc từ _MAX_ mục)",
+            "sInfoPostFix": "",
+            "sUrl": "",
+            "oPaginate": {
+                "sFirst": "Đầu",
+                "sPrevious": "Trước",
+                "sNext": "Tiếp",
+                "sLast": "Cuối"
+            }
+        },
+        "pageLength": 10,
+        "processing": true,
+        "serverSide": true,
+        "ajax": {
+            "url": "https://localhost:44335/user/Get_All_Teacher_No_HomeRoom",
+            "data": function (d) {
+                delete d.columns;
+                d.search = d.search.value;
+                //d.classes = $("#search_class").val();
+                //d.subject = $("#search_subject").val();
+            },
+            "dataSrc": "data"
+        },
+        "rowId": "Id",
+        "initComplete": function () {
+            $('#teacher_table').on('change', '.select-checkbox-teacher', function () {
+                var id = $(this).data('id');
+                if ($(this).prop('checked')) {
+                    if (selectedRowIdClass !== null && selectedRowIdClass !== id) {
+                        // Uncheck previous row if another row is checked
+                        $('#teacher_table').find(`[data-id="${selectedRowIdClass}"]`).prop('checked', false);
+                    }
+                    selectedRowIdClass = id;
+                } else {
+                    selectedRowIdClass = null;
+                }
+                console.log(selectedRowIdClass);
+            });
+            this.api().on('draw', function () {
+                selectedRowIdClass = null;
+                $('#checkAllCheckboxClass').prop('checked', false);
+                $('.select-checkbox-class').prop('checked', false);
+            });
+        },
+        "columns": [
+            {
+                "data": null,
+                "className": "checkbox-column",
+                "orderable": false,
+                "render": function (data) {
+                    return `<input type="checkbox" class="select-checkbox-teacher" data-id="${data.Id}">`;
+                }
+            },
+            { 'data': 'Id', "orderable": false },
+            { 'data': 'Name', "orderable": false },
+            { 'data': 'User_Code', "orderable": false },
+            { 'data': 'Subject_User', "orderable": false },
+            {
+                "data": "AvatarUrl", "className": "img_td", "orderable": false, render: function (data) {
+                    let html = `<img src="${data}" alt="AvatarUrl" />`;
+                    return html;
+                }
+            },
+            {
+                "data": "Status", "orderable": false, render: function (data) {
+                    if (data == 1) return "Hoạt động";
+                    if (data == 2) return "Thất bại";
+                    if (data == 3) return "Ra trường";
+                    if (data == 3) return "Kết thúc";
+                    if (data == 3) return "Đang xử lý";
+                    return "Unknown";
+                }
+            },
+        ],
+        "searching": true,
+        "paging": true,
+        "lengthChange": true,
+        "info": true,
+        "pageLength": 10,
+    });
+}
+function createClass() {
     // Gọi API để lấy dữ liệu
     $.ajax({
         url: 'https://localhost:44335/class/get_list',
