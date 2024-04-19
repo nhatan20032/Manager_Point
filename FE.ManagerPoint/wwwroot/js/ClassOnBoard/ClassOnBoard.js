73be8793c5a6049abe1842725db84c50b1d60373
nhatan20032 <119768785+nhatan20032@users.noreply.github.com>
2024-04-19 11:56:06 +0700

    update show teacher class on semester

--- FE.ManagerPoint/wwwroot/js/ClassOnBoard/ClassOnBoard.js
+++ FE.ManagerPoint/wwwroot/js/ClassOnBoard/ClassOnBoard.js
@@ -1,38 +1,49 @@
 ﻿function GetClassOnBoard(classInfo) {
     if (classInfo && classInfo.length > 0) {
-        var classInfoHtml = '';
-        var subject_InClass = '';
-        var real_subject = '';
+        var classInfo10Html = '';
+        var classInfo11Html = '';
+        var classInfo12Html = '';
+
         classInfo.forEach(item => {
-            item.subjects.forEach(a => {
-                subject_InClass += a.Name + ', ';
-            });
-            real_subject = subject_InClass.slice(0, -2);
-            var statusBadge = '';
-            if (item.status === "Đang giảng dạy") {
-                statusBadge = '<span class="badge rounded-pill bg-success">Đang giảng dạy</span>';
-            } else {
-                statusBadge = '<span class="badge rounded-pill bg-danger">Đã giảng dạy</span>';
-            }
-            classInfoHtml += `
-                    <div class="card mt-3" style="width: 20rem;">
-                                <img class="card-img-top" src="https://th.bing.com/th/id/R.f2e7f87378d459e66b392710a2b78166?rik=2yS%2b0rf6cyD%2b3w&pid=ImgRaw&r=0" alt="${item.classData.Name}">
-                        <div class="card-body">
-                                <h3 class="card-title">Tên lớp: ${item.classData.Name}</h5>
-                                <h5 class="card-title">Mã Lớp: ${item.classData.ClassCode}</h5>
-                                <p class="card-text">Khối: ${item.classData.GradeLevel}</p>
-                                <p class="card-text">Môn dạy: ${real_subject}</p>
-                                <p class="card-text">Giáo viên chủ nhiệm: ${item.homeroom}</p>
-                                <p class="card-text">Trạng thái: ${statusBadge}</p>
-                                <a href="" class="btn btn-primary" ">Vào lớp</a>
-                        </div>
+            var statusBadge = item.status === "Đang giảng dạy" ?
+                '<span class="badge rounded-pill bg-success">Đang giảng dạy</span>' :
+                '<span class="badge rounded-pill bg-danger">Đã giảng dạy</span>';
+
+            var classCardHtml = `
+                <div class="card mt-3" style="width: 20rem;">
+                    <img class="card-img-top" src="https://images.template.net/112327/free-classroom-image-background-0680z.jpg" alt="${item.classData.Name}">
+                    <div class="card-body">
+                        <h3 class="card-title">Tên lớp: ${item.classData.Name}</h3>
+                        <h5 class="card-title">Mã Lớp: ${item.classData.ClassCode}</h5>
+                        <p class="card-text">Khối: ${item.classData.GradeLevel}</p>
+                        <p class="card-text">Trạng thái: ${statusBadge}</p>
+                        <a href="#" class="btn btn-primary">Vào lớp</a>
                     </div>
-                    `;
-            subject_InClass = '';
-            real_subject = '';
+                </div>
+            `;
+
+            switch (item.classData.GradeLevel) {
+                case 10:
+                    classInfo10Html += classCardHtml;
+                    break;
+                case 11:
+                    classInfo11Html += classCardHtml;
+                    break;
+                case 12:
+                    classInfo12Html += classCardHtml;
+                    break;
+                default:
+                    break;
+            }
         });
-        document.getElementById("classInfoDiv").innerHTML = classInfoHtml;
+
+        document.getElementById("classInfo10Div").innerHTML = classInfo10Html || "<div style='text-align: center;'><h5>Không có lớp</h5></div>";
+        document.getElementById("classInfo11Div").innerHTML = classInfo11Html || "<div style='text-align: center;'><h5>Không có lớp</h5></div>";
+        document.getElementById("classInfo12Div").innerHTML = classInfo12Html || "<div style='text-align: center;'><h5>Không có lớp</h5></div>";
+
     } else {
-        document.getElementById("classInfoDiv").innerHTML = "No class information available.";
+        document.getElementById("classInfo10Div").innerHTML = "<h5>No class information available.</h5>";
+        document.getElementById("classInfo11Div").innerHTML = "<h5>No class information available.</h5>";
+        document.getElementById("classInfo12Div").innerHTML = "<h5>No class information available.</h5>";
     }
 }
