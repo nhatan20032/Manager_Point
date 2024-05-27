3271bc66145939b082ebdd9f3988009acac94bfa
nhatan20032 <119768785+nhatan20032@users.noreply.github.com>
2024-05-27 10:27:46 +0700

    update code

--- FE.ManagerPoint/wwwroot/js/site.js
+++ FE.ManagerPoint/wwwroot/js/site.js
@@ -1,4 +1,20 @@
-﻿$("#logout").on("click", function () {
-    sessionStorage.clear();
-    window.location.href = "/Account/Login";
-})
+﻿var roleCode = sessionStorage.getItem('RoleCode');
+if (roleCode !== 'admin') {
+    $('#user_manage').hide();
+    $('#class_manage').hide();
+    $('#course_manage').hide();
+    $('#subject_manage').hide();
+}
+if (roleCode == 'gv') {
+    $('#grade_point').hide();
+}
+if (roleCode == 'hs') {
+    $('#headTeacher').hide();
+    $('#subjectTeacher').hide();
+    $('#analystic').hide();
+}
+if (roleCode == 'admin') {
+    $('#grade_point').hide();
+    $('#headTeacher').hide();
+    $('#subjectTeacher').hide();
+}
