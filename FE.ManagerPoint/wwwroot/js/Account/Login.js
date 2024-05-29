35643ba22822c53322cdfb356c485607ff961af9
nhatan20032 <119768785+nhatan20032@users.noreply.github.com>
2024-05-29 08:51:19 +0700

    update show toasttr for login

--- FE.ManagerPoint/wwwroot/js/Account/Login.js
+++ FE.ManagerPoint/wwwroot/js/Account/Login.js
@@ -1,4 +1,14 @@
-﻿function Login(request) {
+﻿function showLoadingSpinner() {
+    $("#loading-spinner").show();
+    $("#submit").prop("disabled", true);
+}
+
+function hideLoadingSpinner() {
+    $("#loading-spinner").hide();
+    $("#submit").prop("disabled", false);
+}
+function Login(request) {
+    showLoadingSpinner();
     $.ajax({
         url: "https://localhost:44335/user/authenticate",
         method: "POST",
@@ -13,10 +23,18 @@
             sessionStorage.setItem('RoleCode', res.roleCode);
             sessionStorage.setItem('UserRole', res.role);
             sessionStorage.setItem('FullName', res.fullName);
+            hideLoadingSpinner();
             if (sessionStorage.getItem('Token')) {
                 window.location.href = '/Home/Index';
             }
         },
+        error: function (xhr, status, error) {
+            if (xhr.status === 400) {
+                var errorMessage = xhr.responseJSON ? xhr.responseJSON.message : "Bad Request";
+                toastr.error(errorMessage, "Error 400");
+                hideLoadingSpinner();
+            }
+        }
     })
 }
 
